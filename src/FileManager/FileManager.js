74e7b6ecaf409b55a68edb9ca338dfc8db7ccaad
NiNoX14 <hardstuckd5lul@gmail.com>
2022-06-12 15:24:04 +0300

    feat: basic command and change dir

--- src/FileManager/FileManager.js
+++ src/FileManager/FileManager.js
@@ -1,26 +1,27 @@
+import { existsSync, lstatSync, readdirSync } from 'fs';
 import { EOL, homedir } from 'os';
+import { parse, resolve } from 'path';
 import { stdin, stdout } from 'process';
 import { createInterface } from 'readline';
 import { COMMAND_TYPE, ERRORS } from '../constants.js';
 import handleError from '../helpers/handleError.js';
-import logCurrentPath from '../helpers/logCurrentPath.js';
 import parseInputLine from '../helpers/parseInputLine.js';
 import parseUsername from '../helpers/parseUsername.js';
 import onExit from './listeners/onExit.js';
 
 class FileManager {
   userName = parseUsername();
-  currentPath = homedir();
-  logPath = logCurrentPath(this);
+  // currentPath = homedir();
+  _currentPath = `F:\\Projects\\rsSchool\\repos\\nodeJsCourse\\node-file-manager-cli\\.dist`;
 
   basicMethods = {
-    ls: () => {},
-    up: () => {},
-    '.exit': this['.exit'],
+    ls: this.readDir.bind(this),
+    up: () => this.changeDirectory('..'),
+    '.exit': this.exit,
   };
 
   pathMethods = {
-    cd: () => {},
+    cd: this.changeDirectory.bind(this),
     cat: () => {},
     add: () => {},
     rn: () => {},
@@ -51,7 +52,7 @@ class FileManager {
     this.rl.on('line', this.onLine);
   }
 
-  onLine = (input) => {
+  onLine = async (input) => {
     this.logPath();
     const [command, ...args] = parseInputLine(input);
 
@@ -59,8 +60,14 @@ class FileManager {
 
     if (commandType) {
       const method = this.selectMethod(commandType, command, args);
+
+      await this.execute(method, args);
     }
   };
+
+  logPath() {
+    console.log(`You are currently in ${this._currentPath}`);
+  }
 
   validateCommand(command) {
     if (this.pathMethods[command]) {
@@ -80,33 +87,68 @@ class FileManager {
       case COMMAND_TYPE.path: {
         return this.pathMethods[command];
       }
-      case COMMAND_TYPE.os: {
-        return this.selectOsMethod(argsArray);
-      }
       case COMMAND_TYPE.basic: {
         return this.basicMethods[command];
       }
+      case COMMAND_TYPE.os: {
+        return this.selectOsMethod(argsArray);
+      }
     }
   }
+
   selectOsMethod(argsArray) {
     const methodName = argsArray[0];
     const isInvaliArgs = !argsArray.length || argsArray.length > 1;
+    const isMethodExists = !isInvaliArgs && this.osMethods[methodName];
 
-    if (!isInvaliArgs && this.osMethods[methodName]) {
+    if (isMethodExists) {
       return this.osMethods[methodName];
     }
+    return () => handleError(ERRORS.invalidInput);
   }
 
-  async execute(method, args) {
+  async execute(method, argsArray) {
     try {
-      await method(...args);
-    } catch {
+      await method(...argsArray);
+      this.logPath();
+    } catch (e) {
+      console.log(e.message);
       handleError(ERRORS.operationFailed);
     }
   }
 
-  ['.exit']() {
+  exit() {
     process.exit(0);
+  }
+
+  changeDirectory(path, ...args) {
+    if (args.length) {
+      handleError(ERRORS.invalidInput);
+    }
+    const newPath = resolve(this._currentPath, path);
+    const isValidFolder =
+      existsSync(newPath) && lstatSync(newPath).isDirectory();
+    if (isValidFolder) {
+      this._currentPath = newPath;
+    } else {
+      throw new Error('Invalid path');
+    }
+  }
+
+  readDir() {
+    const files = readdirSync(this._currentPath, { withFileTypes: true });
+    if (files.length) {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          console.log(file.name);
+        }
+        if (file.isDirectory()) {
+          console.log(`/${file.name}`);
+        }
+      });
+    } else {
+      console.log('Empty folder.');
+    }
   }
 }
 
--- src/helpers/logCurrentPath.js
+++
@@ -1,5 +0,0 @@
-const logCurrentPath = (FileManager) => () => {
-  console.log(`You are currently in ${FileManager.currentPath}`);
-};
-
-export default logCurrentPath;
